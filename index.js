30171e49c14104be0bb48999ab4c1d48054ae3cf
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-07-30 12:36:48 -0400

    created inquirer prompt

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
+
+
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
@@ -5,6 +21,60 @@ const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [];
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Title:',
+            name: 'title'
+        },
+        {
+            type: 'checkbox',
+            message: 'Choose which license you would like to use:',
+            choices: ['None', 'Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense'],
+            name: 'license'
+        },
+        {
+            type: 'input',
+            message: 'Descriptions of the project:',
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: 'Installation instructions:',
+            name: 'installation'
+        },
+        {
+            type: 'input',
+            message: 'Usage information',
+            name: 'usage'
+        },
+        {
+            type: 'input',
+            message: 'Contribution guidelines:',
+            name: 'contribution'
+        },
+        {
+            type: 'input',
+            message: 'Test instructions:',
+            name: 'test'
+        },
+        {
+            type: 'input',
+            message: 'GitHub profile:',
+            name: 'github'
+        },
+        {
+            type: 'input',
+            message: 'Email:',
+            name: 'email'
+        }
+    ])
+    .then(response => {
+        console.log(response);
+        // generateReadme(response);
+    })
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) { }
